ff8632ff8806edc106e7a98751ab5cfe19badd20
saurabh singh <saurabhsingh.nenosystems@gmail.com>
2020-02-15 20:30:12 +0530

    first commit of fiber web app

---
+++ FiberWebApp/src/main/java/com/fiber/web/app/FiberWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.fiber.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiberWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiberWebAppApplication.class, args);
+	}
+
+}
---
+++ FiberWebApp/src/test/java/com/fiber/web/app/FiberWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiber.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiberWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
